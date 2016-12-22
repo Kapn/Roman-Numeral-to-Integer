5d73bb2135b64346ce3d16f178e03106783547d1
Kevin Pannell <pannellk69@yahoo.com>
2016-12-21 18:37:04 -0800

    finished

--- rnumeral_CodeTest/Program.cs
+++ rnumeral_CodeTest/Program.cs
@@ -20,45 +20,111 @@ namespace rnumeral_CodeTest
         //Calculated correct integer value and returns it.
         public static int romanNum_toInteger(string romanString)
         {
-            int intValue = 0, prevValue = 0, nextValue = 0, curValue = 0;            
+            int intValue = 0, prevValue = -2, nextValue = -1, curValue = 0, highestValue = 0;            
+
+            //check if string is longer than 15 characters, MMMDCCCLXXXVIII is longest possible numeral.
+            if (romanString.Length > 15)
+            {
+                Console.WriteLine("Error, largest roman numeral is only 15 characters");
+                return -1;
+            }
 
             //loop through each character of romanString
             for (int i = 0; i < romanString.Length; i++)
             {
-                //verify that the key exists in the dict
+                //check if more than 3 of the same numerals are in a row
+                if ((i + 3) <= (romanString.Length - 1))
+                {
+                    if (romanDict[romanString[i]] == romanDict[romanString[i + 1]] && romanDict[romanString[i]] == romanDict[romanString[i + 2]])
+                    {
+                        if (romanDict[romanString[i]] == romanDict[romanString[i + 3]])
+                        {
+                            Console.WriteLine("Error, can only be 3 of the same numerals in a row");
+                            return -1;
+                        }
+                    }
+                }
+                //verify that the key exists in the dict.
+                //set curValue if exists.
                 if (romanDict.ContainsKey(romanString[i]))
                 {
                     curValue = romanDict[romanString[i]];
                 } else
                 {
-                    Console.WriteLine("Incorrect Input");
+                    Console.WriteLine("Incorrect Input: " + romanString[i]);
                     return -1;
                 }
-                
+
+                //verify that the next key exists in the dict
+                //set nextValue if exists.
                 if ((i + 1) <= (romanString.Length - 1))
                 {
-                    if(romanDict.ContainsKey(romanString[i + 1]))
+                    if (romanDict.ContainsKey(romanString[i + 1]))
                     {
                         nextValue = romanDict[romanString[i + 1]];
                     }
                     else
                     {
-                        Console.WriteLine("Incorrect Input");
+                        Console.WriteLine("Incorrect Input: " + romanString[i + 1]);
                         return -1;
-                    }                    
+                    }
                 } else
                 {
                     nextValue = 0;
                 }
-                
+
+                //check if incorrect use of roman numerals was used
+                //i.e: VV instead of X
+                if (curValue == nextValue)
+                {
+                    switch (nextValue)
+                    {
+                        case 5:
+                            {
+                                Console.WriteLine("Error, Used 'VV' when could have used 'X'");
+                                return -1;
+                            }
+                        case 50:
+                            {
+                                Console.WriteLine("Error, Used 'LL' when could have used 'C'");
+                                return -1;
+                            }
+                        case 500:
+                            {
+                                Console.WriteLine("Error, Used 'DD' when could have used 'M'");
+                                return -1;
+                            }
+                    }
+                }
+                //check whether correct subtraction is used
                 if (nextValue > curValue)
                 {
-                    intValue += (nextValue - curValue);
-                    i++;
-                }else
+                    //check whether we are adding and then subtracting the same character
+                    if (((i + 2) <= romanString.Length - 1))
+                    {
+                        if (romanDict[romanString[i + 2]] == curValue)
+                        {
+                            Console.WriteLine("Error, subtracting a value, then adding it again");
+                        }
+                    }
+                    //insure correct values are used for subtraction, only I,X,and C are used for subtraction
+                    if (curValue == 100 || curValue == 10 || curValue == 1)
+                    {
+                        intValue += (nextValue - curValue);
+                        i++;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error, can only subtract using I, X, or C. Re-order string");
+                        return -1;
+                    }
+
+                } else
                 {
                     intValue += curValue;
-                }
+                }                            
+
+                //save prevValue for future iterations
                 prevValue = curValue;   
             }
             return intValue;
@@ -75,8 +141,9 @@ namespace rnumeral_CodeTest
             while (!quit)
             {
                 readInString = Console.ReadLine();
+                readInString = readInString.ToUpper();
                 //adjust user input to be lowercase, if the input is 'quit' then quit looping
-                if (readInString.ToLower().Contains("quit"))
+                if (readInString.Contains("QUIT"))
                 {
                     quit = true;
                     break;
--- romanToInt_Test/romanToInt_Tests.cs
+++ romanToInt_Test/romanToInt_Tests.cs
@@ -20,5 +20,37 @@ namespace romanToInt_Test
             actual = Program.romanNum_toInteger(incorrect);
             Assert.AreEqual(expected, actual, 0, "Incorrect Values not failing");
         }
+
+        [TestMethod]
+        public void checkRepitition()
+        {
+            string repitition = "XXXX";
+            int expected = -1;
+            int actual = Program.romanNum_toInteger(repitition);
+            Assert.AreEqual(expected, actual, 0, "incorrect repitition");
+        }
+
+        [TestMethod]
+        public void checkSubtraction()
+        {
+            string correct_sub = "IX";
+            string incorrect_sub = "VX";
+            int expected = 9;
+            int actual = Program.romanNum_toInteger(correct_sub);
+            Assert.AreEqual(expected, actual, 0, "incorrect subtraction");
+
+            expected = -1;
+            actual = Program.romanNum_toInteger(incorrect_sub);
+            Assert.AreEqual(expected, actual, 0, "incorrect subtraction");
+        }
+
+        [TestMethod]
+        public void checkLength()
+        {
+            string toolong = "XXXXXXXXXXXXXXXX";
+            int expected = -1;
+            int actual = Program.romanNum_toInteger(toolong);
+            Assert.AreEqual(expected, actual, 0, "error in length check");
+        }
     }
 }
